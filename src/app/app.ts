baa7d66873555019c12d2ca284e853c702754ccf
Eduard <eduard@nexalab.local>
2026-06-20 21:45:59 -0500

    Initial commit: Landing Page

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('landing-page-parky');
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(open => !open);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
