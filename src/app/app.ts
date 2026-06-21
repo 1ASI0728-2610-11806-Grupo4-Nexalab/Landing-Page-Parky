9d2c9dce8dbf39f2906191601337137849e69567
CellBuuZer <leonardosolis752@gmail.com>
2026-06-20 22:47:27 -0500

    feat: add responsive material form to CTA section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { HowItWorks } from './how-it-works/how-it-works';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [HowItWorks],
+  imports: [HowItWorks, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('landing-page-parky');
   isMenuOpen = signal(false);
 
   toggleMenu() {
-    this.isMenuOpen.update(open => !open);
+    this.isMenuOpen.update((open) => !open);
   }
 
   closeMenu() {
